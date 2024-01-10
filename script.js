1f3857c60e3b2e323becde94f7e70be4f5f14221
LukePadawalker <padurariu.luca@gmail.com>
2024-01-10 16:08:18 +0100

    started creating js logics

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 console.log("JS OK");
 
 
+// recupero la griglia
+// recupero i dati dall'html
+// al click del bottone, genero la griglia
+
+
+const grid = document.querySelector("grid");
+const cell = document.querySelector("cell");
+const easy = document.querySelector("easy");
+const medium = document.querySelector("medium");
+const hard = document.querySelector("hard");
+
+const rows = 10;
+const cols = 10;
+
+
+
